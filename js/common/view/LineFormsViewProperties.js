9e7313a9a9aba95d78d5f48d10c7bc31ffe1f5ec
Chris Malley <cmalley@pixelzoom.com>
2013-08-23 16:21:15 -0600

    port some of the common.view

---
+++ js/common/view/LineFormsViewProperties.js
@@ -0,0 +1,24 @@
+// Copyright 2002-2013, University of Colorado
+
+/**
+ * Properties that are specific to the view for the "Slope", "Slope-Intercept" and "Point-Slope" tabs.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+define( function( require ) {
+  'use strict';
+
+  // imports
+  var PropertySet = require( 'AXON/PropertySet' );
+
+  function LineFormsViewProperties() {
+    PropertySet.call( this, {
+      linesVisible: true,
+      interactiveLineVisible: true,
+      interactiveEquationVisible: true,
+      slopeVisible: true
+    } );
+  }
+
+  return LineFormsViewProperties;
+} );

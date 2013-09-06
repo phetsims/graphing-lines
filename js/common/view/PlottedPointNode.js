be3e707c79706d062e32063a77f4a538cf4dd204
Chris Malley <cmalley@pixelzoom.com>
2013-09-05 21:33:17 -0600

    use diameter instead of radius, as in Java implementation

--- js/common/view/PlottedPointNode.js
+++ js/common/view/PlottedPointNode.js
@@ -12,11 +12,11 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var ShadedSphereNode = require( 'SCENERY_PHET/ShadedSphereNode' );
 
-  function PlottedPointNode( radius, color, options ) {
+  function PlottedPointNode( diameter, color, options ) {
     options = _.extend( { lineWidth: 1, pickable: false }, options );
     options.outerColor = color;
     options.stroke = options.outerColor;
-    ShadedSphereNode.call( this, radius, options );
+    ShadedSphereNode.call( this, diameter, options );
   }
 
   return inherit( ShadedSphereNode, PlottedPointNode );

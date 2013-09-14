12d204eea130fe17e9a35047445a6c8a2dbde7ef
Chris Malley <cmalley@pixelzoom.com>
2013-09-13 20:09:05 -0600

    add icon for slope tool to control panel

--- js/common/view/IconFactory.js
+++ js/common/view/IconFactory.js
@@ -10,6 +10,12 @@ define( function( require ) {
 
   // imports
   var GLColors = require( 'GRAPHING_LINES/common/GLColors' );
+  var Line = require( 'GRAPHING_LINES/common/model/Line' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Path = require( 'SCENERY/nodes/Path' );
+  var Shape = require( 'KITE/Shape' );
+  var SlopeInterceptModel = require( 'GRAPHING_LINES/slopeintercept/model/SlopeInterceptModel' );
+  var SlopeToolNode = require( 'GRAPHING_LINES/common/view/SlopeToolNode' );
 
   //TODO this should go away
   var createRectangleIcon = function( width, fill, stroke ) {
@@ -23,10 +29,30 @@ define( function( require ) {
   };
 
   return {
+
     // Creates an icon for the slope-tool feature
     createSlopeToolIcon: function( width ) {
-      //TODO placeholder, port this from java.SlopeToolNode
-      return createRectangleIcon( width, 'blue', 'black' );
+
+      var parentNode = new Node();
+
+      var model = new SlopeInterceptModel();
+      model.interactiveLineProperty.set( Line.createSlopeIntercept( 1, 2, 0 ) ); // bigger values will make slope tool look smaller in icon
+
+      // slope tool
+      var slopeToolNode = new SlopeToolNode( model.interactiveLineProperty, model.mvt );
+      parentNode.addChild( slopeToolNode );
+
+      // dashed line where the line would be, tweaked visually
+      var lineNode = new Path( Shape.lineSegment( slopeToolNode.left + ( 0.4 * slopeToolNode.width ), slopeToolNode.bottom,
+        slopeToolNode.right, slopeToolNode.top + ( 0.5 * slopeToolNode.height ) ),
+        { lineWidth: 1,
+          lineDash: [ 6, 6 ],
+          stroke: 'black'
+        } );
+      parentNode.addChild( lineNode );
+
+      parentNode.scale( width / parentNode.width );
+      return parentNode;//TODO convert to image
     },
 
     // Creates an icon for the "y = +x" feature

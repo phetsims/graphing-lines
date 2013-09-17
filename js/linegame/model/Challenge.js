1184729ac085c6d65d6c2de7d6aedcf84db8ef33
Chris Malley <cmalley@pixelzoom.com>
2013-09-17 16:02:02 -0600

    port more of the Line Game model

--- js/linegame/model/Challenge.js
+++ js/linegame/model/Challenge.js
@@ -24,7 +24,7 @@ define( function( require ) {
    * @param {String} title title that is visible to the user
    * @param {String} description brief description of the challenge, visible in dev versions
    * @param {Line} answer the correct answer
-   * @param {EquationForm} equationForm specifies the form of the equations
+   * @param {EquationForm} equationForm specifies the form of the equation
    * @param {ManipulationMode} manipulationMode indicates which properties of a line the user is able to change
    * @param {Range} xRange range of the graph's x axis
    * @param {Range} yRange range of the graph's y axis
---
+++ js/linegame/model/GraphTheLine.js
@@ -0,0 +1,71 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+/**
+ * Model for "Graph the Line" challenges.
+ * In this challenge, the user is given an equation and must graph the line.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // imports
+  var GLStrings = require( 'GRAPHING_LINES/common/GLStrings' );
+  var GraphTheLineNode = require( 'GRAPHING_LINES/linegame/view/GraphTheLineNode' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Challenge = require( 'GRAPHING_LINES/linegame/model/Challenge' );
+  var Vector2 = require( 'DOT/Vector2' );
+
+  /**
+   * @param {String} description brief description of the challenge, visible in dev versions
+   * @param {Line} answer  the correct answer
+   * @param {EquationForm} equationForm specifies the form of the equation
+   * @param {ManipulationMode} manipulationMode indicates which properties of a line the user is able to change
+   * @param {Range} xRange range of the graph's x axis
+   * @param {Range} yRange range of the graph's y axis
+   * @constructor
+   */
+  function GraphTheLine( description, answer, equationForm, manipulationMode, xRange, yRange ) {
+    Challenge.call( this,
+      Challenge.createTitle( GLStrings.graphTheLine, manipulationMode ),
+      description,
+      answer,
+      equationForm,
+      manipulationMode,
+      xRange,
+      yRange,
+      new Vector2( 735, 300 ), /* origin offset */
+      new Vector2( xRange.min + ( 0.65 * xRange.getLength() ), yRange.min - 1 ), /* point tool location 1 */
+      new Vector2( xRange.min + ( 0.95 * xRange.getLength() ), yRange.min - 4 ) /* point tool location 2 */
+    );
+  }
+
+  return inherit( Challenge, GraphTheLine, {
+
+    /**
+     * Creates the view for this challenge.
+     * @override
+     * @param {LineGameModel} model the game model
+     * @param {Dimension2} challengeSize dimensions of the view rectangle that is available for rendering the challenge
+     * @param {GameAudioPlayer} audioPlayer the audio player, for providing audio feedback during game play
+     */
+    createView: function( model, challengeSize, audioPlayer ) {
+      return new GraphTheLineNode( this, model, challengeSize, audioPlayer );
+    },
+
+    /**
+     * Updates the collection of lines that are "seen" by the point tools.
+     * @override
+     */
+    updateGraphLines: function() {
+      this.graph.lines.clear();
+      // add lines in the order that they would be rendered
+      if ( this.guess.get() ) {
+        this.graph.lines.add( this.guess.get() );
+      }
+      if ( this.answerVisible ) {
+        this.graph.lines.add( this.answer );
+      }
+    }
+  } );
+} );

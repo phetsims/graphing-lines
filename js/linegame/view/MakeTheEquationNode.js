5ecea97fe7d27f6573716aad0f3df0c6b1e02f82
Chris Malley <cmalley@pixelzoom.com>
2013-09-24 14:23:03 -0600

    more porting of linegame.view package

--- js/linegame/view/MakeTheEquationNode.js
+++ js/linegame/view/MakeTheEquationNode.js
@@ -10,9 +10,22 @@ define( function( require ) {
   'use strict';
 
   // imports
+  var AnswerGraphNode = require( 'GRAPHING_LINES/linegame/view/AnswerGraphNode' );
   var ChallengeNode = require( 'GRAPHING_LINES/linegame/view/ChallengeNode' );
+  var Color = require( 'SCENERY/util/Color' );
+  var Dimension2 = require( 'DOT/Dimension2' );
+  var EquationBoxNode = require( 'GRAPHING_LINES/linegame/view/EquationBoxNode' );
+  var EquationForm = require( 'GRAPHING_LINES/linegame/model/EquationForm' );
+  var GLStrings = require( 'GRAPHING_LINES/common/GLStrings' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var LineGameConstants = require( 'GRAPHING_LINES/linegame/LineGameConstants' );
+  var ManipulationMode = require( 'GRAPHING_LINES/linegame/model/ManipulationMode' );
   var Node = require( 'SCENERY/nodes/Node' );
+  var PlayState = require( 'GRAPHING_LINES/linegame/model/PlayState' );
+  var PointSlopeEquationNode = require( 'GRAPHING_LINES/pointslope/view/PointSlopeEquationNode' );
+  var Property = require( 'AXON/Property' );
+  var SlopeInterceptEquationNode = require( 'GRAPHING_LINES/slopeintercept/view/SlopeInterceptEquationNode' );
+  var Text = require( 'SCENERY/nodes/Text' );
 
   /**
    * @param {GraphTheLine} challenge
@@ -21,146 +34,133 @@ define( function( require ) {
    * @param {GameAudioPlayer} audioPlayer
    * @constructor
    */
-  function MakeTheEquationNode(  challenge, model, challengeSize, audioPlayer ) {
+  function MakeTheEquationNode( challenge, model, challengeSize, audioPlayer ) {
 
     var thisNode = this;
     ChallengeNode.call( thisNode, challenge, model, challengeSize, audioPlayer );
+
+    // title, possibly scaled for i18n
+    var titleNode = new Text( challenge.title, { font: LineGameConstants.TITLE_FONT, fill: LineGameConstants.TITLE_COLOR } );
+    var maxTitleWidth = 0.45 * challengeSize.width;
+    if ( titleNode.width > maxTitleWidth ) {
+      titleNode.scale = maxTitleWidth / titleNode.width;
+    }
+
+    var boxSize = new Dimension2( 0.4 * challengeSize.width, 0.3 * challengeSize.height );
+
+    // Answer
+    var answerBoxNode =
+      new EquationBoxNode( GLStrings.aCorrectEquation, challenge.answer.color, boxSize,
+        ChallengeNode.createEquationNode( challenge.equationForm, challenge.answer, LineGameConstants.STATIC_EQUATION_FONT, challenge.answer.color ) );
+    answerBoxNode.visible = false;
+
+    // Guess
+    var guessBoxNode =
+      new EquationBoxNode( GLStrings.yourEquation, challenge.guess.get().color, boxSize,
+        createInteractiveEquationNode( challenge.equationForm, challenge.manipulationMode, challenge.guess, challenge.graph,
+          LineGameConstants.INTERACTIVE_EQUATION_FONT, LineGameConstants.STATIC_EQUATION_FONT, challenge.guess.get().color ) );
+
+    // Graph
+    var graphNode = new AnswerGraphNode( challenge );
+
+    // rendering order
+    thisNode.subclassParent.addChild( titleNode );
+    thisNode.subclassParent.addChild( graphNode );
+    thisNode.subclassParent.addChild( answerBoxNode );
+    thisNode.subclassParent.addChild( guessBoxNode );
+
+    // layout
+    {
+      // graphNode is positioned automatically based on mvt's origin offset.
+
+      // guess equation in left half of challenge space
+      guessBoxNode.right = ( 0.5 * challengeSize.width ) - 50;
+      guessBoxNode.bottom = challenge.mvt.modelToViewY( 0 ) - 10;
+
+      // answer below guess
+      answerBoxNode.left = guessBoxNode.left;
+      answerBoxNode.top = challenge.mvt.modelToViewY( 0 ) + 10;
+
+      // face centered below equation boxes
+      thisNode.faceNode.centerX = answerBoxNode.centerX;
+      thisNode.faceNode.bottom = thisNode.checkButton.bottom;
+
+      // title above guess equation, left justified
+      titleNode.left = guessBoxNode.left;
+      titleNode.bottom = guessBoxNode.top = 20;
+    }
+
+    // To reduce brain damage during development, show the answer equation in translucent gray.
+    if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
+      var devAnswerNode = ChallengeNode.createEquationNode( challenge.equationForm, challenge.answer, LineGameConstants.STATIC_EQUATION_FONT, new Color( 0, 0, 0, 25 ) );
+      devAnswerNode.left = answerBoxNode.left + 30;
+      devAnswerNode.centerY = answerBoxNode.centerY;
+      thisNode.addChild( devAnswerNode );
+      devAnswerNode.moveToBack();
+    }
+
+    // Update visibility of the correct/incorrect icons.
+    var updateIcons = function() {
+      answerBoxNode.setCorrectIconVisible( model.playStateProperty.get() === PlayState.NEXT );
+      guessBoxNode.setCorrectIconVisible( model.playStateProperty.get() === PlayState.NEXT && challenge.isCorrect() );
+      guessBoxNode.setIncorrectIconVisible( model.playStateProperty.get() === PlayState.NEXT && !challenge.isCorrect() );
+    };
+
+    // sync with guess
+    challenge.guess.link( updateIcons.bind( thisNode ) );
+
+    // sync with game state
+    model.playStateProperty.link( function( state ) {
+
+      // states in which the equation is interactive
+      guessBoxNode.pickable = ( state === PlayState.FIRST_CHECK || state === PlayState.SECOND_CHECK || ( state === PlayState.NEXT && !challenge.isCorrect() ) );
+
+      // Graph the guess line at the end of the challenge.
+      graphNode.setGuessVisible( state === PlayState.NEXT );
+
+      // show stuff when the user got the challenge wrong
+      if ( state === PlayState.NEXT && !challenge.isCorrect() ) {
+        answerBoxNode.setVisible( true );
+        graphNode.setAnswerPointVisible( true );
+        graphNode.setGuessPointVisible( true );
+        graphNode.setSlopeToolVisible( true );
+      }
+
+      // visibility of correct/incorrect icons
+      updateIcons();
+    } );
   }
+
+  /**
+   * Creates an interactive equation.
+   * @param {EquationForm} equationForm
+   * @param {ManipulationMode} manipulationMode
+   * @param {Property<Line>} line
+   * @param {Graph} graph
+   * @param {Font} interactiveFont
+   * @param {Font} staticFont
+   * @param {Color} staticColor
+   */
+   var createInteractiveEquationNode = function( equationForm, manipulationMode, line, graph, interactiveFont, staticFont, staticColor ) {
+    var interactivePoint, interactiveSlope, interactiveIntercept;
+    if ( equationForm === EquationForm.SLOPE_INTERCEPT ) {
+      interactiveSlope = ( manipulationMode === ManipulationMode.SLOPE ) || ( manipulationMode === ManipulationMode.SLOPE_INTERCEPT );
+      interactiveIntercept = ( manipulationMode === ManipulationMode.INTERCEPT ) || ( manipulationMode === ManipulationMode.SLOPE_INTERCEPT );
+      return new SlopeInterceptEquationNode( line,
+        new Property( graph.yRange ), new Property( graph.xRange ), new Property( graph.yRange ),
+        interactiveSlope, interactiveIntercept, interactiveFont, staticFont, staticColor );
+    }
+    else if ( equationForm === EquationForm.POINT_SLOPE ) {
+      interactivePoint = ( manipulationMode == ManipulationMode.POINT ) || ( manipulationMode == ManipulationMode.POINT_SLOPE );
+      interactiveSlope = ( manipulationMode == ManipulationMode.SLOPE ) || ( manipulationMode == ManipulationMode.POINT_SLOPE );
+      return new PointSlopeEquationNode( line, new Property( graph.xRange ),
+        new Property( graph.yRange ), new Property( graph.yRange ), new Property( graph.xRange ),
+        interactivePoint, interactivePoint, interactiveSlope, interactiveFont, staticFont, staticColor );
+    }
+    else {
+      throw new Error( "unsupported equation form: " + equationForm );
+    }
+  };
 
   return inherit( ChallengeNode, MakeTheEquationNode );
 } );
-
-
-//        // title, possibly scaled for i18n
-//        PNode titleNode = new PhetPText( challenge.title, LineGameConstants.TITLE_FONT, LineGameConstants.TITLE_COLOR );
-//        final double maxTitleWidth = 0.45 * challengeSize.getWidth();
-//        if ( titleNode.getFullBoundsReference().getWidth() > maxTitleWidth ) {
-//            titleNode.scale( maxTitleWidth / titleNode.getFullBoundsReference().getWidth() );
-//        }
-//
-//        final PDimension boxSize = new PDimension( 0.4 * challengeSize.getWidth(), 0.3 * challengeSize.getHeight() );
-//
-//        // Answer
-//        final EquationBoxNode answerBoxNode =
-//                new EquationBoxNode( Strings.A_CORRECT_EQUATION, challenge.answer.color, boxSize,
-//                                     createEquationNode( challenge.equationForm, challenge.answer, LineGameConstants.STATIC_EQUATION_FONT, challenge.answer.color ) );
-//        answerBoxNode.setVisible( false );
-//
-//        // Guess
-//        final EquationBoxNode guessBoxNode =
-//                new EquationBoxNode( Strings.YOUR_EQUATION, challenge.guess.get().color, boxSize,
-//                                     createInteractiveEquationNode( challenge.equationForm, challenge.manipulationMode, challenge.guess, challenge.graph,
-//                                                                    LineGameConstants.INTERACTIVE_EQUATION_FONT, LineGameConstants.STATIC_EQUATION_FONT,
-//                                                                    challenge.guess.get().color ) );
-//
-//        // Graph
-//        final ChallengeGraphNode graphNode = new AnswerGraphNode( challenge );
-//
-//        // rendering order
-//        subclassParent.addChild( titleNode );
-//        subclassParent.addChild( graphNode );
-//        subclassParent.addChild( answerBoxNode );
-//        subclassParent.addChild( guessBoxNode );
-//
-//        // layout
-//        {
-//            // graphNode is positioned automatically based on mvt's origin offset.
-//
-//            // guess equation in left half of challenge space
-//            guessBoxNode.setOffset( ( 0.5 * challengeSize.getWidth() ) - ( guessBoxNode.getFullBoundsReference().getWidth() ) - 50,
-//                                    challenge.mvt.modelToViewY( 0 ) - guessBoxNode.getFullBoundsReference().getHeight() - 10 );
-//
-//            // answer below guess
-//            answerBoxNode.setOffset( guessBoxNode.getXOffset(),
-//                                     challenge.mvt.modelToViewY( 0 ) + 10 );
-//
-//            // face centered below equation boxes
-//            faceNode.setOffset( answerBoxNode.getFullBoundsReference().getCenterX() - ( faceNode.getFullBoundsReference().getWidth() / 2 ),
-//                                checkButton.getFullBoundsReference().getMaxY() - faceNode.getFullBoundsReference().getHeight() );
-//
-//            // title above guess equation, left justified
-//            titleNode.setOffset( guessBoxNode.getFullBoundsReference().getMinX(),
-//                                 guessBoxNode.getFullBoundsReference().getMinY() - titleNode.getFullBoundsReference().getHeight() - 20 );
-//
-//        }
-//
-//        // To reduce brain damage during development, show the answer equation in translucent gray.
-//        if ( PhetApplication.getInstance().isDeveloperControlsEnabled() ) {
-//            PNode devAnswerNode = createEquationNode( challenge.equationForm, challenge.answer, LineGameConstants.STATIC_EQUATION_FONT, new Color( 0, 0, 0, 25 ) );
-//            devAnswerNode.setOffset( answerBoxNode.getFullBoundsReference().getMinX() + 30,
-//                                     answerBoxNode.getFullBoundsReference().getCenterY() - ( devAnswerNode.getFullBoundsReference().getHeight() / 2 ) );
-//            addChild( devAnswerNode );
-//            devAnswerNode.moveToBack();
-//        }
-//
-//        // Update visibility of the correct/incorrect icons.
-//        final VoidFunction0 updateIcons = new VoidFunction0() {
-//            public void apply() {
-//                answerBoxNode.setCorrectIconVisible( model.state.get() == PlayState.NEXT );
-//                guessBoxNode.setCorrectIconVisible( model.state.get() == PlayState.NEXT && challenge.isCorrect() );
-//                guessBoxNode.setIncorrectIconVisible( model.state.get() == PlayState.NEXT && !challenge.isCorrect() );
-//            }
-//        };
-//
-//        // sync with guess
-//        challenge.guess.addObserver( new SimpleObserver() {
-//            public void update() {
-//                // visibility of correct/incorrect icons
-//                updateIcons.apply();
-//            }
-//        } );
-//
-//        // sync with game state
-//        model.state.addObserver( new VoidFunction1<PlayState>() {
-//            public void apply( PlayState state ) {
-//
-//                // states in which the equation is interactive
-//                guessBoxNode.setPickable( state == PlayState.FIRST_CHECK || state == PlayState.SECOND_CHECK || ( state == PlayState.NEXT && !challenge.isCorrect() ) );
-//                guessBoxNode.setChildrenPickable( guessBoxNode.getPickable() );
-//
-//                // Graph the guess line at the end of the challenge.
-//                graphNode.setGuessVisible( state == PlayState.NEXT );
-//
-//                // show stuff when the user got the challenge wrong
-//                if ( state == PlayState.NEXT && !challenge.isCorrect() ) {
-//                    answerBoxNode.setVisible( true );
-//                    graphNode.setAnswerPointVisible( true );
-//                    graphNode.setGuessPointVisible( true );
-//                    graphNode.setSlopeToolVisible( true );
-//                }
-//
-//                // visibility of correct/incorrect icons
-//                updateIcons.apply();
-//            }
-//        } );
-//    }
-//
-//    // Creates an interactive equation.
-//    private EquationNode createInteractiveEquationNode( EquationForm equationForm, ManipulationMode manipulationMode,
-//                                                        Property<Line> line, Graph graph, PhetFont interactiveFont, PhetFont staticFont, Color staticColor ) {
-//        if ( equationForm == EquationForm.SLOPE_INTERCEPT ) {
-//            boolean interactiveSlope = ( manipulationMode == ManipulationMode.SLOPE ) || ( manipulationMode == ManipulationMode.SLOPE_INTERCEPT );
-//            boolean interactiveIntercept = ( manipulationMode == ManipulationMode.INTERCEPT ) || ( manipulationMode == ManipulationMode.SLOPE_INTERCEPT );
-//            return new SlopeInterceptEquationNode( line,
-//                                                   new Property<DoubleRange>( new DoubleRange( graph.yRange ) ),
-//                                                   new Property<DoubleRange>( new DoubleRange( graph.xRange ) ),
-//                                                   new Property<DoubleRange>( new DoubleRange( graph.yRange ) ),
-//                                                   interactiveSlope, interactiveIntercept,
-//                                                   interactiveFont, staticFont, staticColor );
-//        }
-//        else if ( equationForm == EquationForm.POINT_SLOPE ) {
-//            boolean interactivePoint = ( manipulationMode == ManipulationMode.POINT ) || ( manipulationMode == ManipulationMode.POINT_SLOPE );
-//            boolean interactiveSlope = ( manipulationMode == ManipulationMode.SLOPE ) || ( manipulationMode == ManipulationMode.POINT_SLOPE );
-//            return new PointSlopeEquationNode( line,
-//                                               new Property<DoubleRange>( new DoubleRange( graph.xRange ) ),
-//                                               new Property<DoubleRange>( new DoubleRange( graph.yRange ) ),
-//                                               new Property<DoubleRange>( new DoubleRange( graph.yRange ) ),
-//                                               new Property<DoubleRange>( new DoubleRange( graph.xRange ) ),
-//                                               interactivePoint, interactivePoint, interactiveSlope,
-//                                               interactiveFont, staticFont, staticColor );
-//        }
-//        else {
-//            throw new IllegalArgumentException( "unsupported equation form: " + equationForm );
-//        }
-//    }
-//}

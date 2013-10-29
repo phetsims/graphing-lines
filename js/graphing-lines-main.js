23e05459417726c7d6d49002dfb1574eed887776
Chris Malley <cmalley@pixelzoom.com>
2013-10-29 12:04:34 -0600

    joist#69, convert to different requirejs style for main.js

--- js/graphing-lines-main.js
+++ js/graphing-lines-main.js
@@ -5,38 +5,41 @@
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
-require( [
-  'JOIST/SimLauncher',
-  'JOIST/Sim',
-  'GRAPHING_LINES/slope/SlopeScreen',
-  'GRAPHING_LINES/slopeIntercept/SlopeInterceptScreen',
-  'GRAPHING_LINES/pointSlope/PointSlopeScreen',
-  'GRAPHING_LINES/linegame/LineGameScreen',
-  'string!GRAPHING_LINES/graphing-lines.name' ],
-  function( SimLauncher, Sim, SlopeScreen, SlopeInterceptScreen, PointSlopeScreen, LineGameScreen, simTitle ) {
-    'use strict';
+define( function( require ) {
+  'use strict';
 
-    //TODO i18n, see joist#66
-    var simOptions = {
-      credits: 'PhET Development Team -\n' +
-               'Lead Design: Ariel Paul\n' +
-               'Software Development: Chris Malley\n' +
-               'Design Team: Karina Hensberry, Patricia Loeblein, Kathy Perkins'
-    };
+  // imports
+  var LineGameScreen = require( 'GRAPHING_LINES/linegame/LineGameScreen' );
+  var PointSlopeScreen = require( 'GRAPHING_LINES/pointSlope/PointSlopeScreen' );
+  var Sim = require( 'JOIST/Sim' );
+  var SimLauncher = require( 'JOIST/SimLauncher' );
+  var SlopeInterceptScreen = require( 'GRAPHING_LINES/slopeIntercept/SlopeInterceptScreen' );
+  var SlopeScreen = require( 'GRAPHING_LINES/slope/SlopeScreen' );
 
-    // Appending '?dev' to the URL will enable developer-only features.
-    if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
-      simOptions = _.extend( {
-        // add dev-specific options here
-        showHomeScreen: false,
-        screenIndex: 3
-      }, simOptions );
-    }
+  // strings
+  var simTitle = require( 'string!GRAPHING_LINES/graphing-lines.name' );
 
-    SimLauncher.launch( function() {
-      var sim = new Sim( simTitle, [
-        new SlopeScreen(), new SlopeInterceptScreen(), new PointSlopeScreen(), new LineGameScreen() ],
-        simOptions );
-      sim.start();
-    } );
+  //TODO i18n, see joist#66
+  var simOptions = {
+    credits: 'PhET Development Team -\n' +
+             'Lead Design: Ariel Paul\n' +
+             'Software Development: Chris Malley\n' +
+             'Design Team: Karina Hensberry, Patricia Loeblein, Kathy Perkins'
+  };
+
+  // Appending '?dev' to the URL will enable developer-only features.
+  if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
+    simOptions = _.extend( {
+      // add dev-specific options here
+      showHomeScreen: false,
+      screenIndex: 3
+    }, simOptions );
+  }
+
+  SimLauncher.launch( function() {
+    var sim = new Sim( simTitle, [
+      new SlopeScreen(), new SlopeInterceptScreen(), new PointSlopeScreen(), new LineGameScreen() ],
+      simOptions );
+    sim.start();
   } );
+} );

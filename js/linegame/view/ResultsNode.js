2d092d268c8aec6701aa5c3328ce2ca76add211b
Aadish Gupta <aadish@digitalgreen.org>
2016-04-19 10:00:30 -0600

    updating dispose function of reward node and calling dispose from all the simulations using it, see https://github.com/phetsims/vegas/issues/52

--- js/linegame/view/ResultsNode.js
+++ js/linegame/view/ResultsNode.js
@@ -69,6 +69,9 @@ define( function( require ) {
       }
       else {
         thisNode.removeAllChildren();
+        if ( thisNode.rewardNode !== null ) {
+          thisNode.rewardNode.dispose();
+        }
         thisNode.rewardNode = null;
       }
     } );

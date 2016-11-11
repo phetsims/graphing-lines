0485db046e9a7f64f62398dab4d4d4e33b107968
Jonathan Olson <olsonsjc@gmail.com>
2016-11-11 11:36:04 -0700

    Simplifying Rectangle.setRect() so that it doesn't override corner radii if they aren't provided. Cleaned up setRect/setRectBounds usages. See https://github.com/phetsims/scenery/issues/576

--- js/common/view/NumberBackgroundNode.js
+++ js/common/view/NumberBackgroundNode.js
@@ -39,7 +39,12 @@ define( function( require ) {
 
     // text and background
     var textNode = new Text( '?', { fill: options.textFill, font: options.font } ); // @private
-    var backgroundNode = new Rectangle( 0, 0, 1, 1, { fill: options.backgroundFill, stroke: options.backgroundStroke } ); // @private
+    // @private
+    var backgroundNode = new Rectangle( 0, 0, 1, 1, {
+      fill: options.backgroundFill,
+      stroke: options.backgroundStroke,
+      cornerRadius: options.cornerRadius
+    } );
     options.children = [ backgroundNode, textNode ];
     Node.call( this, options );
 
@@ -51,7 +56,7 @@ define( function( require ) {
       // adjust the background to fit the value
       var backgroundWidth = Math.max( options.minWidth, textNode.width + options.xMargin + options.xMargin );
       var backgroundHeight = Math.max( options.minHeight, textNode.height + options.yMargin + options.yMargin );
-      backgroundNode.setRect( 0, 0, backgroundWidth, backgroundHeight, options.cornerRadius, options.cornerRadius );
+      backgroundNode.setRect( 0, 0, backgroundWidth, backgroundHeight );
 
       // center the value in the background
       textNode.centerX = backgroundNode.centerX;

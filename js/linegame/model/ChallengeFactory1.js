54c52f7fc9e86389ce9bdd39d8e5facb99b0e74a
Chris Malley <cmalley@pixelzoom.com>
2017-03-14 20:52:50 -0600

    convert ChallengeFactory1 to ValuePool, phetsims/graphing-slope-intercept#8

--- js/linegame/model/ChallengeFactory1.js
+++ js/linegame/model/ChallengeFactory1.js
@@ -21,6 +21,7 @@ define( function( require ) {
   var ManipulationMode = require( 'GRAPHING_LINES/linegame/model/ManipulationMode' );
   var Range = require( 'DOT/Range' );
   var RandomChooser = require( 'GRAPHING_LINES/linegame/model/RandomChooser' );
+  var ValuePool = require( 'GRAPHING_LINES/linegame/model/ValuePool' );
   var Vector2 = require( 'DOT/Vector2' );
 
   /**
@@ -43,104 +44,54 @@ define( function( require ) {
      */
     createChallenges: function() {
 
-      // all variables, manually hoisted
       var challenges = [];
-      var x;
-      var y;
-      var x1Range;
-      var y1Range;
-      var yInterceptRange;
-      var pointArrays;
-      var pointArrayIndices;
-      var quadrant1Points;
-      var quadrant3Points;
-      var slopeArrays;
-      var slopeArrayIndices;
-      var yInterceptArrays;
-      var yInterceptArrayIndices;
-      var pointSlopeManipulationModes;
+
+      // hoist vars
       var slope;
       var yIntercept;
       var point;
       var description;
       var manipulationMode;
 
-      var xRange = this.xRange;
-      var yRange = this.yRange;
-
-      // for point manipulation challenges, (x1,y1) must be in Quadrant 1 (both coordinates positive) or Quadrant 3 (both coordinates negative)
-      {
-        x1Range = new Range( -9, 4 );
-        y1Range = new Range( -9, 4 );
-        assert && assert( xRange.containsRange( x1Range ) && yRange.containsRange( y1Range ) );
-
-        // all points in Quadrant 1
-        quadrant1Points = [];
-        for ( x = 1; x < xRange.max; x++ ) {
-          for ( y = 1; y < yRange.max; y++ ) {
-            quadrant1Points.push( new Vector2( x, y ) );
-          }
-        }
-
-        // all points in Quadrant 3
-        quadrant3Points = [];
-        for ( x = x1Range.min; x < 0; x++ ) {
-          for ( y = y1Range.min; y < 0; y++ ) {
-            quadrant3Points.push( new Vector2( x, y ) );
-          }
-        }
-
-        pointArrays = [ quadrant1Points, quadrant3Points ];
-        pointArrayIndices = RandomChooser.rangeToArray( new Range( 0, pointArrays.length - 1 ) );
-      }
-
-      // for slope manipulation challenges, 1 slope must come from each list
-      slopeArrays = [
-        [ new Fraction( 3, 2 ), new Fraction( 4, 3 ), new Fraction( 5, 2 ), new Fraction( 5, 3 ) ],
-        [ new Fraction( 1, 2 ), new Fraction( 1, 3 ), new Fraction( 1, 4 ), new Fraction( 1, 5 ) ],
-        [ new Fraction( 2, 3 ), new Fraction( 3, 4 ), new Fraction( 3, 5 ), new Fraction( 2, 5 ) ]
-      ];
-      slopeArrayIndices = RandomChooser.rangeToArray( new Range( 0, slopeArrays.length - 1 ) );
-
-      // for y-intercept manipulation challenges, one must be positive, one negative
-      yInterceptRange = new Range( -6, 4 );
-      assert && assert( yRange.containsRange( yInterceptRange ) );
-      yInterceptArrays = [
-        RandomChooser.rangeToArray( new Range( yInterceptRange.min, -1 ) ),
-        RandomChooser.rangeToArray( new Range( 1, yInterceptRange.max ) )
-      ];
-      yInterceptArrayIndices = RandomChooser.rangeToArray( new Range( 0, yInterceptArrays.length - 1 ) );
-
-      // for point-slope form, one of each manipulation mode
-      pointSlopeManipulationModes = [ ManipulationMode.POINT, ManipulationMode.SLOPE ];
+      // pools of values for points, slope and y-intercept
+      var pointPool = new ValuePool( this.createPointArrays() );
+      var slopePool = new ValuePool( this.createSlopeArrays() );
+      var yInterceptPool = new ValuePool( this.createYInterceptArrays() );
 
       // CHALLENGE 1: Graph-the-Line, slope-intercept form, slope variable
-      slope = RandomChooser.chooseFromArrays( slopeArrays, slopeArrayIndices ); // first required slope
-      yIntercept = RandomChooser.chooseFromArrays( yInterceptArrays ); // unique y-intercept
-      challenges.push( new GraphTheLine( '1 of 3 required slopes',
+      slope = slopePool.chooseRequired();
+      yIntercept = yInterceptPool.chooseOptional();
+      challenges.push( new GraphTheLine( 'required slopes, slope variable',
         Line.createSlopeIntercept( slope.numerator, slope.denominator, yIntercept ),
-        EquationForm.SLOPE_INTERCEPT, ManipulationMode.SLOPE, xRange, yRange ) );
+        EquationForm.SLOPE_INTERCEPT, ManipulationMode.SLOPE,
+        this.xRange, this.yRange ) );
 
       // CHALLENGE 2: Graph-the-Line, slope-intercept form, intercept variable
-      slope = RandomChooser.chooseFromArrays( slopeArrays ); // unique slope
-      yIntercept = RandomChooser.chooseFromArrays( yInterceptArrays, yInterceptArrayIndices ); // first required y-intercept, unique
-      challenges.push( new GraphTheLine( '1 of 2 required y-intercepts',
+      slope = slopePool.chooseOptional();
+      yIntercept = yInterceptPool.chooseRequired();
+      challenges.push( new GraphTheLine( 'required y-intercept, y-intercept variable',
         Line.createSlopeIntercept( slope.numerator, slope.denominator, yIntercept ),
-        EquationForm.SLOPE_INTERCEPT, ManipulationMode.INTERCEPT, xRange, yRange ) );
+        EquationForm.SLOPE_INTERCEPT, ManipulationMode.INTERCEPT,
+        this.xRange, this.yRange ) );
 
       // CHALLENGE 3: Make-the-Equation, slope-intercept form, slope variable
-      slope = RandomChooser.chooseFromArrays( slopeArrays, slopeArrayIndices ); // second required slope, unique
-      yIntercept = RandomChooser.chooseFromArrays( yInterceptArrays ); // unique y-intercept
-      challenges.push( new MakeTheEquation( '2 of 3 required slopes',
+      slope = slopePool.chooseRequired();
+      yIntercept = yInterceptPool.chooseOptional();
+      challenges.push( new MakeTheEquation( 'required slope, slope variable',
         Line.createSlopeIntercept( slope.numerator, slope.denominator, yIntercept ),
-        EquationForm.SLOPE_INTERCEPT, ManipulationMode.SLOPE, xRange, yRange ) );
+        EquationForm.SLOPE_INTERCEPT, ManipulationMode.SLOPE,
+        this.xRange, this.yRange ) );
 
       // CHALLENGE 4: Make-the-Equation, slope-intercept form, intercept variable
-      slope = RandomChooser.chooseFromArrays( slopeArrays ); // unique slope
-      yIntercept = RandomChooser.chooseFromArrays( yInterceptArrays, yInterceptArrayIndices ); // second required y-intercept, unique
-      challenges.push( new MakeTheEquation( '2 of 2 required y-intercepts',
+      slope = slopePool.chooseOptional();
+      yIntercept = yInterceptPool.chooseRequired();
+      challenges.push( new MakeTheEquation( 'required y-intercept, y-intercept variable',
         Line.createSlopeIntercept( slope.numerator, slope.denominator, yIntercept ),
-        EquationForm.SLOPE_INTERCEPT, ManipulationMode.INTERCEPT, xRange, yRange ) );
+        EquationForm.SLOPE_INTERCEPT, ManipulationMode.INTERCEPT,
+        this.xRange, this.yRange ) );
+
+      // for point-slope form, one of each manipulation mode
+      var pointSlopeManipulationModes = [ ManipulationMode.POINT, ManipulationMode.SLOPE ];
 
       // CHALLENGE 5: Graph-the-Line, point-slope form, point or slope variable (random choice)
       {
@@ -148,20 +99,21 @@ define( function( require ) {
         manipulationMode = RandomChooser.choose( pointSlopeManipulationModes );
 
         if ( manipulationMode === ManipulationMode.SLOPE ) {
-          point = RandomChooser.chooseFromArrays( pointArrays ); // unique point
-          slope = RandomChooser.chooseFromArrays( slopeArrays, slopeArrayIndices ); // third required slope, unique
-          description = 'random choice to manipulate slope, 3 of 3 required slopes';
+          point = pointPool.chooseOptional();
+          slope = slopePool.chooseRequired(); // third required slope, unique
+          description = 'required slope, slope variable';
         }
         else {
-          point = RandomChooser.chooseFromArrays( pointArrays, pointArrayIndices ); // first required point, unique
-          slope = RandomChooser.chooseFromArrays( slopeArrays ); // unique slope
-          description = 'random choice to manipulate point, 1 of 2 required points';
+          point = pointPool.chooseRequired();
+          slope = slopePool.chooseOptional();
+          description = 'require point, point variable';
         }
 
         // challenge
         challenges.push( new GraphTheLine( description,
           Line.createPointSlope( point.x, point.y, slope.numerator, slope.denominator ),
-          EquationForm.POINT_SLOPE, manipulationMode, xRange, yRange ) );
+          EquationForm.POINT_SLOPE, manipulationMode,
+          this.xRange, this.yRange ) );
       }
 
       // CHALLENGE 6: Make-the-Equation, point-slope form, point or slope variable (whichever was not chosen above)
@@ -170,23 +122,84 @@ define( function( require ) {
         manipulationMode = RandomChooser.choose( pointSlopeManipulationModes );
 
         if ( manipulationMode === ManipulationMode.SLOPE ) {
-          point = RandomChooser.chooseFromArrays( pointArrays ); // unique point
-          slope = RandomChooser.chooseFromArrays( slopeArrays, slopeArrayIndices ); // third required slope, unique
-          description = 'manipulate slope because Graph-the-Line uses point, 3 of 3 required slopes';
+          point = pointPool.chooseOptional();
+          slope = slopePool.chooseRequired();
+          description = 'required slope, slope variable';
         }
         else {
-          point = RandomChooser.chooseFromArrays( pointArrays, pointArrayIndices ); // second required point, unique
-          slope = RandomChooser.chooseFromArrays( slopeArrays ); // unique slope
-          description = 'manipulate point because Graph-the-Line uses slope, 2 of 2 required points';
+          point = pointPool.chooseRequired();
+          slope = slopePool.chooseOptional();
+          description = 'required point, point variable';
         }
 
         // challenge
         challenges.push( new MakeTheEquation( description,
           Line.createPointSlope( point.x, point.y, slope.numerator, slope.denominator ),
-          EquationForm.POINT_SLOPE, manipulationMode, xRange, yRange ) );
+          EquationForm.POINT_SLOPE, manipulationMode,
+          this.xRange, this.yRange ) );
       }
 
       return challenges;
+    },
+
+    /**
+     * Creates the points used in point manipulation challenges.
+     * (x1,y1) must be in Quadrant 1 (both coordinates positive) or Quadrant 3 (both coordinates negative)
+     * @returns {Vector2[][]}
+     */
+    createPointArrays: function() {
+
+      var x1Range = new Range( -9, 4 );
+      var y1Range = new Range( -9, 4 );
+      assert && assert( this.xRange.containsRange( x1Range ) && this.yRange.containsRange( y1Range ) );
+
+      var x;
+      var y;
+
+      // all points in Quadrant 1
+      var quadrant1Points = [];
+      for ( x = 1; x < this.xRange.max; x++ ) {
+        for ( y = 1; y < this.yRange.max; y++ ) {
+          quadrant1Points.push( new Vector2( x, y ) );
+        }
+      }
+
+      // all points in Quadrant 3
+      var quadrant3Points = [];
+      for ( x = x1Range.min; x < 0; x++ ) {
+        for ( y = y1Range.min; y < 0; y++ ) {
+          quadrant3Points.push( new Vector2( x, y ) );
+        }
+      }
+
+      return [ quadrant1Points, quadrant3Points ];
+    },
+
+    /**
+     * Creates the sets of slopes used for generating challenges.
+     * @returns {Fraction[][]}
+     * @protected
+     */
+    createSlopeArrays: function() {
+      return [
+        [ new Fraction( 3, 2 ), new Fraction( 4, 3 ), new Fraction( 5, 2 ), new Fraction( 5, 3 ) ],
+        [ new Fraction( 1, 2 ), new Fraction( 1, 3 ), new Fraction( 1, 4 ), new Fraction( 1, 5 ) ],
+        [ new Fraction( 2, 3 ), new Fraction( 3, 4 ), new Fraction( 3, 5 ), new Fraction( 2, 5 ) ]
+      ];
+    },
+
+    /**
+     * Creates the sets of y-intercepts used for generating challenges.
+     * @returns {number[][]}
+     * @protected
+     */
+    createYInterceptArrays: function() {
+      var yRangeSubset = new Range( -6, 4 );
+      assert && assert( this.yRange.containsRange( yRangeSubset ), 'values are out of range' );
+      return [
+        ValuePool.rangeToArray( new Range( yRangeSubset.min, -1 ) ), // negative intercepts
+        ValuePool.rangeToArray( new Range( 1, yRangeSubset.max ) )   // positive intercepts
+      ];
     }
   } );
 } );

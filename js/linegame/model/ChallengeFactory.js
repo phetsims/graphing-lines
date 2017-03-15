4e76d9be869ab315e11d3c29ffc1cf98c958a36c
Chris Malley <cmalley@pixelzoom.com>
2017-03-14 22:14:37 -0600

    copyright date

--- js/linegame/model/ChallengeFactory.js
+++ js/linegame/model/ChallengeFactory.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2015, University of Colorado Boulder
+// Copyright 2013-2017, University of Colorado Boulder
 
 /**
  * Base type for challenge factories in the 'Graphing Lines' sim.

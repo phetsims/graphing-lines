3b016d0a86724eef08b07c83d19cc0a8ece25a97
Chris Malley <cmalley@pixelzoom.com>
2017-04-23 13:13:41 -0600

    add @constructor

--- js/common/view/DimensionalArrowNode.js
+++ js/common/view/DimensionalArrowNode.js
@@ -31,6 +31,7 @@ define( function( require ) {
    * @param {number} tipX
    * @param {number} tipY
    * @param {Object} [options]
+   * @constructor
    */
   function DimensionalArrowNode( tailX, tailY, tipX, tipY, options ) {
 

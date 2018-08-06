3ee1a60c6fbc114dd20aca50336e10806e5ed6c6
Chris Malley <cmalley@pixelzoom.com>
2018-08-06 13:24:49 -0600

    GQViewProperties extends LineFormsViewProperties, instead of adding irrelevant Properties to LineFormsViewProperties, https://github.com/phetsims/graphing-quadratics/issues/7
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/view/LineFormsViewProperties.js
+++ js/common/view/LineFormsViewProperties.js
@@ -9,7 +9,6 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var BooleanProperty = require( 'AXON/BooleanProperty' );
   var graphingLines = require( 'GRAPHING_LINES/graphingLines' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Property = require( 'AXON/Property' );
@@ -30,27 +29,6 @@ define( function( require ) {
 
     // @public determines whether the slope tool is visible on the graph
     this.slopeToolVisibleProperty = new Property( true );
-
-    // @public graphing-quadratics: determines whether a point is displayed to mark the vertex of the quadratic
-    this.vertexVisibleProperty = new BooleanProperty( false );
-
-    // @public graphing-quadratics: determines whether axis of symmetry on the quadratic is displayed
-    this.axisOfSymmetryVisibleProperty = new BooleanProperty( false );
-
-    // @public graphing-quadratics: determines points are displayed to mark roots of the quadratic
-    this.rootsVisibleProperty = new BooleanProperty( false );
-
-    // @public graphing-quadratics: determines whether a curve is displayed to reflect the quadratic term (y=ax^2)
-    this.quadraticTermVisibleProperty = new BooleanProperty( false );
-
-    // @public graphing-quadratics: determines whether a line is displayed to reflect the linear term (y=bx)
-    this.linearTermVisibleProperty = new BooleanProperty( false );
-
-    // @public graphing-quadratics: determines whether a line is displayed to reflect the constant term (y=c)
-    this.constantTermVisibleProperty = new BooleanProperty( false );
-
-    // @public graphing-quadratics: determines whether a point and a line are displayed to mark the focus and directrix
-    this.directrixVisibleProperty = new BooleanProperty( false );
   }
 
   graphingLines.register( 'LineFormsViewProperties', LineFormsViewProperties );
@@ -63,13 +41,6 @@ define( function( require ) {
       this.gridVisibleProperty.reset();
       this.interactiveEquationVisibleProperty.reset();
       this.slopeToolVisibleProperty.reset();
-      this.vertexVisibleProperty.reset();
-      this.axisOfSymmetryVisibleProperty.reset();
-      this.rootsVisibleProperty.reset();
-      this.quadraticTermVisibleProperty.reset();
-      this.linearTermVisibleProperty.reset();
-      this.constantTermVisibleProperty.reset();
-      this.directrixVisibleProperty.reset();
     }
   } );
 } );

b5598036938ff4f132ff466a2841c182db62ba8e
Chris Malley <cmalley@pixelzoom.com>
2019-02-12 16:32:29 -0700

    i18n fix for game guesses with undefined slope, #117

--- js/pointslope/view/PointSlopeEquationNode.js
+++ js/pointslope/view/PointSlopeEquationNode.js
@@ -146,9 +146,7 @@ define( function( require ) {
       x1Node = new DynamicValueNode( x1Property, _.extend( { absoluteValue: true }, staticOptions ) );
     }
     var xRightParenNode = new Text( ')', staticOptions );
-    var slopeUndefinedNode = new RichText( '?', _.extend( {}, staticOptions, {
-      maxWidth: options.maxWidth // see #114
-    } ) );
+    var slopeUndefinedNode = new RichText( '?', staticOptions );
 
     // add all nodes, we'll set which ones are visible bases on desired simplification
     self.children = [

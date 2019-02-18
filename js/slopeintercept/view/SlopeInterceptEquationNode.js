e66a53b2ef54b5b6d67d8969f569a9e316e5fa49
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-18 14:21:44 -0700

    Update copyright year, see https://github.com/phetsims/tasks/issues/988

--- js/slopeintercept/view/SlopeInterceptEquationNode.js
+++ js/slopeintercept/view/SlopeInterceptEquationNode.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2018, University of Colorado Boulder
+// Copyright 2013-2019, University of Colorado Boulder
 
 /**
  * Renderer for slope-intercept equations, with optional interactivity of slope and intercept.

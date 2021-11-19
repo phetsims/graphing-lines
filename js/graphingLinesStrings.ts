4ffd935cdde50601b8cd6e83182fa189c1927480
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:58 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/graphingLinesStrings.ts
@@ -0,0 +1,55 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import graphingLines from './graphingLines.js';
+
+type StringsType = {
+  'graphing-lines': {
+    'title': string
+  },
+  'eraseLines': string,
+  'saveLine': string,
+  'slope': string,
+  'slopeUndefined': string,
+  'graphTheLine': string,
+  'makeTheEquation': string,
+  'setTheSlope': string,
+  'setTheYIntercept': string,
+  'setThePoint': string,
+  'putPointsOnLine': string,
+  'lineToGraph': string,
+  'yourLine': string,
+  'yourEquation': string,
+  'aCorrectEquation': string,
+  'undefined': string,
+  'notALine': string,
+  'symbol': {
+    'x': string,
+    'y': string,
+    'slope': string,
+    'intercept': string
+  },
+  'screen': {
+    'slope': string,
+    'slopeIntercept': string,
+    'pointSlope': string,
+    'lineGame': string
+  },
+  'point': {
+    'XY': string,
+    'unknown': string
+  },
+  'slopeIs': string,
+  'chooseYourLevel': string,
+  'pattern_Level_0': string
+};
+
+const graphingLinesStrings = getStringModule( 'GRAPHING_LINES' ) as StringsType;
+
+graphingLines.register( 'graphingLinesStrings', graphingLinesStrings );
+
+export default graphingLinesStrings;

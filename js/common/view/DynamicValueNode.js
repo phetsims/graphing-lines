db2991052dffd662c52fbf379007cce2fafe7310
phet-dev <phetadmin@gmail.com>
2021-11-26 03:17:36 -0700

    update copyright dates from daily grunt work

--- js/common/view/DynamicValueNode.js
+++ js/common/view/DynamicValueNode.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2020, University of Colorado Boulder
+// Copyright 2013-2021, University of Colorado Boulder
 
 /**
  * Text node that stays synchronized with a dynamic value. This is used in interactive equations,
--- js/common/view/manipulator/X2Y2Manipulator.js
+++ js/common/view/manipulator/X2Y2Manipulator.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2020, University of Colorado Boulder
+// Copyright 2013-2021, University of Colorado Boulder
 
 /**
  * Manipulator for changing a line's (x2,y2) point.

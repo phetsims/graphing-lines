8bf339a322f92b2b62a35dc994f95d98082d6756
pixelzoom <cmalley@pixelzoom.com>
2022-07-11 13:49:08 -0600

    factor out LineGameLevelSelectionButtonGroup, https://github.com/phetsims/vegas/issues/108

---
+++ js/linegame/view/LineGameLevelSelectionButtonGroup.js
@@ -0,0 +1,97 @@
+// Copyright 2022, University of Colorado Boulder
+
+/**
+ * LineGameLevelSelectionButtonGroup is the group of level-selection buttons for the 'Line Game' screen.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+
+import merge from '../../../../phet-core/js/merge.js';
+import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
+import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
+import { AlignBox, AlignGroup, Image, Text, VBox } from '../../../../scenery/js/imports.js';
+import LevelSelectionButtonGroup from '../../../../vegas/js/LevelSelectionButtonGroup.js';
+import ScoreDisplayStars from '../../../../vegas/js/ScoreDisplayStars.js';
+import graphingLines from '../../graphingLines.js';
+import graphingLinesStrings from '../../graphingLinesStrings.js';
+import GamePhase from '../model/GamePhase.js';
+
+class LineGameLevelSelectionButtonGroup extends LevelSelectionButtonGroup {
+
+  /**
+   * @param {LineGameModel} model
+   * @param {HTMLImageElement[]} levelImages - images for the level-selection buttons, ordered by level
+   * @param {Object} [options]
+   */
+  constructor( model, levelImages, options ) {
+    assert && assert( levelImages.length === model.numberOfLevels, 'one image is required for each game level' );
+
+    options = merge( {
+      levelSelectionButtonOptions: {
+        baseColor: 'rgb( 180, 205, 255 )',
+        buttonWidth: 175,
+        buttonHeight: 210,
+        bestTimeVisibleProperty: model.timerEnabledProperty
+      },
+      flowBoxOptions: {
+        spacing: 50
+      }
+    }, options );
+
+    // To give all button icons the same effective size
+    const iconAlignGroup = new AlignGroup();
+
+    // Descriptions of LevelSelectionButtons
+    const levelSelectionButtonItems = [];
+    for ( let level = 0; level < model.numberOfLevels; level++ ) {
+      levelSelectionButtonItems.push( {
+        icon: createLevelSelectionButtonIcon( level, levelImages[ level ], iconAlignGroup ),
+        scoreProperty: model.bestScoreProperties[ level ],
+        options: {
+          bestTimeProperty: model.bestTimeProperties[ level ],
+          bestTimeVisibleProperty: model.timerEnabledProperty,
+          createScoreDisplay: scoreProperty => new ScoreDisplayStars( scoreProperty, {
+            numberOfStars: model.challengesPerGameProperty.get(),
+            perfectScore: model.getPerfectScore( level )
+          } ),
+          listener: () => {
+            model.levelProperty.set( level );
+            model.setGamePhase( GamePhase.PLAY );
+          },
+          soundPlayerIndex: level
+        }
+      } );
+    }
+
+    super( levelSelectionButtonItems, options );
+  }
+}
+
+/**
+ * Creates an icon for a LevelSelectionButton.
+ * @param {number} level
+ * @param {HTMLImageElement} levelImage
+ * @param {AlignGroup} iconAlignGroup
+ * @returns {Node}
+ */
+function createLevelSelectionButtonIcon( level, levelImage, iconAlignGroup ) {
+
+  // Level N
+  const label = new Text( StringUtils.format( graphingLinesStrings.pattern_Level_0, level + 1 ), {
+    font: new PhetFont( 40 ),
+    maxWidth: 100
+  } );
+
+  const image = new AlignBox( new Image( levelImage, { scale: 0.54 } ), {
+    group: iconAlignGroup
+  } );
+
+  // 'Level N' centered above image
+  return new VBox( {
+    spacing: 15,
+    children: [ label, image ]
+  } );
+}
+
+graphingLines.register( 'LineGameLevelSelectionButtonGroup', LineGameLevelSelectionButtonGroup );
+export default LineGameLevelSelectionButtonGroup;
--- js/linegame/view/SettingsNode.js
+++ js/linegame/view/SettingsNode.js
@@ -8,17 +8,14 @@
  */
 
 import merge from '../../../../phet-core/js/merge.js';
-import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import ResetAllButton from '../../../../scenery-phet/js/buttons/ResetAllButton.js';
 import TimerToggleButton from '../../../../scenery-phet/js/buttons/TimerToggleButton.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
-import { AlignBox, AlignGroup, Image, Node, Text, VBox } from '../../../../scenery/js/imports.js';
-import LevelSelectionButtonGroup from '../../../../vegas/js/LevelSelectionButtonGroup.js';
-import ScoreDisplayStars from '../../../../vegas/js/ScoreDisplayStars.js';
+import { Node, Text, VBox } from '../../../../scenery/js/imports.js';
 import GLConstants from '../../common/GLConstants.js';
 import graphingLines from '../../graphingLines.js';
 import graphingLinesStrings from '../../graphingLinesStrings.js';
-import GamePhase from '../model/GamePhase.js';
+import LineGameLevelSelectionButtonGroup from './LineGameLevelSelectionButtonGroup.js';
 
 class SettingsNode extends Node {
 
@@ -33,17 +30,7 @@ class SettingsNode extends Node {
     assert && assert( levelImages.length === model.numberOfLevels, 'one image is required for each game level' );
 
     options = merge( {
-      levelSelectionButtonGroupOptions: {
-        levelSelectionButtonOptions: {
-          baseColor: 'rgb( 180, 205, 255 )',
-          buttonWidth: 175,
-          buttonHeight: 210,
-          bestTimeVisibleProperty: model.timerEnabledProperty
-        },
-        flowBoxOptions: {
-          spacing: 50
-        }
-      }
+      levelSelectionButtonGroupOptions: {}
     }, options );
 
     // Title
@@ -52,34 +39,8 @@ class SettingsNode extends Node {
       maxWidth: 0.85 * layoutBounds.width
     } );
 
-    // To give all button icons the same effective size
-    const iconAlignGroup = new AlignGroup();
-
-    // Descriptions of LevelSelectionButtons
-    const levelSelectionButtonItems = [];
-    for ( let level = 0; level < model.numberOfLevels; level++ ) {
-      levelSelectionButtonItems.push( {
-        icon: createLevelSelectionButtonIcon( level, levelImages[ level ], iconAlignGroup ),
-        scoreProperty: model.bestScoreProperties[ level ],
-        options: {
-          bestTimeProperty: model.bestTimeProperties[ level ],
-          bestTimeVisibleProperty: model.timerEnabledProperty,
-          createScoreDisplay: scoreProperty => new ScoreDisplayStars( scoreProperty, {
-            numberOfStars: model.challengesPerGameProperty.get(),
-            perfectScore: model.getPerfectScore( level )
-          } ),
-          listener: () => {
-            model.levelProperty.set( level );
-            model.setGamePhase( GamePhase.PLAY );
-          },
-          soundPlayerIndex: level
-        }
-      } );
-    }
-
     // Group of LevelSelectionButtons
-    const levelSelectionButtonGroup =
-      new LevelSelectionButtonGroup( levelSelectionButtonItems, options.levelSelectionButtonGroupOptions );
+    const levelSelectionButtonGroup = new LineGameLevelSelectionButtonGroup( model, levelImages, options.levelSelectionButtonGroupOptions );
 
     // Timer and Sound controls
     const timerToggleButton = new TimerToggleButton( model.timerEnabledProperty, {
@@ -112,32 +73,6 @@ class SettingsNode extends Node {
 
     super( options );
   }
-}
-
-/**
- * Creates an icon for a LevelSelectionButton.
- * @param {number} level
- * @param {HTMLImageElement} levelImage
- * @param {AlignGroup} iconAlignGroup
- * @returns {Node}
- */
-function createLevelSelectionButtonIcon( level, levelImage, iconAlignGroup ) {
-
-  // Level N
-  const label = new Text( StringUtils.format( graphingLinesStrings.pattern_Level_0, level + 1 ), {
-    font: new PhetFont( 40 ),
-    maxWidth: 100
-  } );
-
-  const image = new AlignBox( new Image( levelImage, { scale: 0.54 } ), {
-    group: iconAlignGroup
-  } );
-
-  // 'Level N' centered above image
-  return new VBox( {
-    spacing: 15,
-    children: [ label, image ]
-  } );
 }
 
 graphingLines.register( 'SettingsNode', SettingsNode );

4c0acae0f7d5e345ef46a7cd4e987a3b359d74e5
pixelzoom <cmalley@pixelzoom.com>
2022-07-19 15:50:58 -0600

    use MathSymbolFont.getRichTextMarkup where appropriate in my sims

--- js/common/GLSymbols.js
+++ js/common/GLSymbols.js
@@ -6,19 +6,15 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import StringUtils from '../../../phetcommon/js/util/StringUtils.js';
 import MathSymbolFont from '../../../scenery-phet/js/MathSymbolFont.js';
 import graphingLines from '../graphingLines.js';
 import graphingLinesStrings from '../graphingLinesStrings.js';
 
-// constants
-const SYMBOL_PATTERN = `<i style='font-family: ${new MathSymbolFont( 10 ).family}'>{{symbol}}</i>`;
-
 const GLSymbols = {
-  b: StringUtils.fillIn( SYMBOL_PATTERN, { symbol: graphingLinesStrings.symbol.intercept } ),
-  m: StringUtils.fillIn( SYMBOL_PATTERN, { symbol: graphingLinesStrings.symbol.slope } ),
-  x: StringUtils.fillIn( SYMBOL_PATTERN, { symbol: graphingLinesStrings.symbol.x } ),
-  y: StringUtils.fillIn( SYMBOL_PATTERN, { symbol: graphingLinesStrings.symbol.y } )
+  b: MathSymbolFont.getRichTextMarkup( graphingLinesStrings.symbol.intercept ),
+  m: MathSymbolFont.getRichTextMarkup( graphingLinesStrings.symbol.slope ),
+  x: MathSymbolFont.getRichTextMarkup( graphingLinesStrings.symbol.x ),
+  y: MathSymbolFont.getRichTextMarkup( graphingLinesStrings.symbol.y )
 };
 
 graphingLines.register( 'GLSymbols', GLSymbols );

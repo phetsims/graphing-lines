b5bba0288cbfed853ab1380fe006982a58b268ba
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:39 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/graphingLinesStrings.ts
+++ js/graphingLinesStrings.ts
@@ -5,47 +5,78 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import graphingLines from './graphingLines.js';
 
 type StringsType = {
   'graphing-lines': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'eraseLines': string;
+  'eraseLinesProperty': TReadOnlyProperty<string>;
   'saveLine': string;
+  'saveLineProperty': TReadOnlyProperty<string>;
   'slope': string;
+  'slopeProperty': TReadOnlyProperty<string>;
   'slopeUndefined': string;
+  'slopeUndefinedProperty': TReadOnlyProperty<string>;
   'graphTheLine': string;
+  'graphTheLineProperty': TReadOnlyProperty<string>;
   'makeTheEquation': string;
+  'makeTheEquationProperty': TReadOnlyProperty<string>;
   'setTheSlope': string;
+  'setTheSlopeProperty': TReadOnlyProperty<string>;
   'setTheYIntercept': string;
+  'setTheYInterceptProperty': TReadOnlyProperty<string>;
   'setThePoint': string;
+  'setThePointProperty': TReadOnlyProperty<string>;
   'putPointsOnLine': string;
+  'putPointsOnLineProperty': TReadOnlyProperty<string>;
   'lineToGraph': string;
+  'lineToGraphProperty': TReadOnlyProperty<string>;
   'yourLine': string;
+  'yourLineProperty': TReadOnlyProperty<string>;
   'yourEquation': string;
+  'yourEquationProperty': TReadOnlyProperty<string>;
   'aCorrectEquation': string;
+  'aCorrectEquationProperty': TReadOnlyProperty<string>;
   'undefined': string;
+  'undefinedProperty': TReadOnlyProperty<string>;
   'notALine': string;
+  'notALineProperty': TReadOnlyProperty<string>;
   'symbol': {
     'x': string;
+    'xProperty': TReadOnlyProperty<string>;
     'y': string;
+    'yProperty': TReadOnlyProperty<string>;
     'slope': string;
+    'slopeProperty': TReadOnlyProperty<string>;
     'intercept': string;
+    'interceptProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'slope': string;
+    'slopeProperty': TReadOnlyProperty<string>;
     'slopeIntercept': string;
+    'slopeInterceptProperty': TReadOnlyProperty<string>;
     'pointSlope': string;
+    'pointSlopeProperty': TReadOnlyProperty<string>;
     'lineGame': string;
+    'lineGameProperty': TReadOnlyProperty<string>;
   };
   'point': {
     'XY': string;
+    'XYProperty': TReadOnlyProperty<string>;
     'unknown': string;
+    'unknownProperty': TReadOnlyProperty<string>;
   };
   'slopeIs': string;
+  'slopeIsProperty': TReadOnlyProperty<string>;
   'chooseYourLevel': string;
+  'chooseYourLevelProperty': TReadOnlyProperty<string>;
   'pattern_Level_0': string;
+  'pattern_Level_0Property': TReadOnlyProperty<string>;
 };
 
 const graphingLinesStrings = getStringModule( 'GRAPHING_LINES' ) as StringsType;

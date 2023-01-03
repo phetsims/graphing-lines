61886a973038b5bd8d2362fdf0f655d5ba1fe02c
phet-dev <phetadmin@gmail.com>
2023-01-03 04:12:45 -0700

    update copyright dates from daily grunt work

--- js/linegame/model/ValuePool.ts
+++ js/linegame/model/ValuePool.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2022, University of Colorado Boulder
+// Copyright 2017-2023, University of Colorado Boulder
 
 /**
  * ValuePool takes sets of values, separates them into "required" and "optional" sets,

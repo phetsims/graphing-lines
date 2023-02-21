5fecf56779531fd3a7108ce88fa54b74555b50ca
pixelzoom <cmalley@pixelzoom.com>
2023-02-21 14:24:20 -0700

    convert to TypeScript

--- js/common/view/PointToolBodyNode.ts
+++ js/common/view/PointToolBodyNode.ts
@@ -1,65 +1,88 @@
 // Copyright 2021-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * PointToolBodyNode is the body portion of the point tool. It includes everything except the tool's probe.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import Utils from '../../../../dot/js/Utils.js';
-import merge from '../../../../phet-core/js/merge.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import optionize from '../../../../phet-core/js/optionize.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import ShadedRectangle from '../../../../scenery-phet/js/ShadedRectangle.js';
-import { Color, LinearGradient, Node, Rectangle, RichText, VBox } from '../../../../scenery/js/imports.js';
+import { Color, LinearGradient, Node, NodeOptions, Rectangle, RichText, TColor, VBox } from '../../../../scenery/js/imports.js';
 import graphingLines from '../../graphingLines.js';
 import GraphingLinesStrings from '../../GraphingLinesStrings.js';
 import GLColors from '../GLColors.js';
 
 // constants
-const COORDINATES_SIDE_VALUES = [ 'left', 'right' ];
 const DEFAULT_FONT = new PhetFont( { size: 15, weight: 'bold' } );
 const NUMBER_OF_GRIPS = 3;
 
-class PointToolBodyNode extends Node {
+// The side where the client intends to add a probe
+type CoordinatesSide = 'left' | 'right';
 
-  /**
-   * @param {Property.<Vector2>} coordinatesProperty
-   * @param {Object} [options]
-   */
-  constructor( coordinatesProperty, options ) {
+type SelfOptions = {
 
-    options = merge( {
+  // The side where the client intends to add a probe
+  coordinatesSide?: CoordinatesSide;
 
-      // The side where the client intends to add a probe, see COORDINATES_SIDE_VALUES
+  // For both the body and text background
+  cornerRadius?: number;
+
+  // body
+  bodyWidth?: number;
+  bodyHeight?: number;
+  bodyColor?: TColor;
+  bodyXMargin?: number;
+
+  // text background
+  backgroundWidth?: number;
+  backgroundHeight?: number;
+  backgroundFill?: TColor;
+  backgroundStroke?: TColor;
+  backgroundXMargin?: number;
+  backgroundYMargin?: number;
+
+  // text
+  font?: PhetFont;
+  textFill?: TColor;
+  decimals?: number;
+};
+
+type PointToolBodyNodeOptions = SelfOptions;
+
+export default class PointToolBodyNode extends Node {
+
+  private readonly background: Rectangle;
+  private readonly textNode: RichText;
+  private readonly disposePointToolBodyNode: () => void;
+
+  public constructor( coordinatesProperty: TReadOnlyProperty<Vector2 | null>, providedOptions?: PointToolBodyNodeOptions ) {
+
+    const options = optionize<PointToolBodyNodeOptions, SelfOptions, NodeOptions>()( {
+
+      // SelfOptions
       coordinatesSide: 'left',
-
-      // For both the body and text background
       cornerRadius: 8,
-
-      // body
       bodyWidth: 130,
       bodyHeight: 48,
       bodyColor: GLColors.POINT_TOOL_COLOR,
       bodyXMargin: 7,
-
-      // text background
       backgroundWidth: 70,
       backgroundHeight: 35,
       backgroundFill: 'white',
       backgroundStroke: Color.grayColor( 185 ),
       backgroundXMargin: 4,
       backgroundYMargin: 2,
-
-      // text
       font: DEFAULT_FONT,
       textFill: 'black',
       decimals: 1
-    }, options );
-
-    assert && assert( COORDINATES_SIDE_VALUES.includes( options.coordinatesSide ) );
+    }, providedOptions );
 
     // The main rectangle
     const bodyRectangle = new ShadedRectangle( new Bounds2( 0, 0, options.bodyWidth, options.bodyHeight ), {
@@ -124,7 +147,7 @@ class PointToolBodyNode extends Node {
     super( options );
 
     // Update the displayed coordinates. Use toFixedNumber so that trailing zeros are removed.
-    const coordinatesListener = coordinates => {
+    const coordinatesListener = ( coordinates: Vector2 | null ) => {
       if ( coordinates ) {
         const x = Utils.toFixedNumber( coordinates.x, options.decimals );
         const y = Utils.toFixedNumber( coordinates.y, options.decimals );
@@ -141,45 +164,34 @@ class PointToolBodyNode extends Node {
       textNode.center = background.center;
     } );
 
-    // @private
     this.disposePointToolBodyNode = () => {
       if ( coordinatesProperty.hasListener( coordinatesListener ) ) {
         coordinatesProperty.unlink( coordinatesListener );
       }
     };
 
-    // @private
     this.background = background;
     this.textNode = textNode;
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     this.disposePointToolBodyNode();
     super.dispose();
   }
 
   /**
    * Sets the fill for the text.
-   * @param {ColorDef} fill
-   * @public
    */
-  setTextFill( fill ) {
+  public setTextFill( fill: TColor ): void {
     this.textNode.fill = fill;
   }
 
   /**
    * Sets the fill for the text background.
-   * @param {ColorDef} fill
-   * @public
    */
-  setBackgroundFill( fill ) {
+  public setBackgroundFill( fill: TColor ): void {
     this.background.fill = fill;
   }
 }
 
 graphingLines.register( 'PointToolBodyNode', PointToolBodyNode );
-export default PointToolBodyNode;

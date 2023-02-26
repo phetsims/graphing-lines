6c79b0eadfaeba3e8c1873e7d03fe56f4dc7c4c1
pixelzoom <cmalley@pixelzoom.com>
2023-02-26 07:56:10 -0700

    convert to TypeScript

--- js/common/view/PointToolProbeNode.ts
+++ js/common/view/PointToolProbeNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2021-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * PointToolProbeNode is the probe for the point tool. It's a triangle that points down, described from the upper-left.
  * Origin is at the upper-left.
@@ -10,31 +9,39 @@
 
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import { Shape } from '../../../../kite/js/imports.js';
-import merge from '../../../../phet-core/js/merge.js';
-import { Color, Path } from '../../../../scenery/js/imports.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+import PickOptional from '../../../../phet-core/js/types/PickOptional.js';
+import { Color, NodeTranslationOptions, Path, PathOptions } from '../../../../scenery/js/imports.js';
 import graphingLines from '../../graphingLines.js';
 import GLColors from '../GLColors.js';
 
+type SelfOptions = {
+  size?: Dimension2;
+};
+
+type PointToolProbeNodeOptions = SelfOptions & NodeTranslationOptions & PickOptional<PathOptions, 'pickable'>;
+
 export default class PointToolProbeNode extends Path {
 
-  /**
-   * @param {Object} [options]
-   */
-  constructor( options ) {
+  public constructor( providedOptions?: PointToolProbeNodeOptions ) {
 
-    options = merge( {
+    const options = optionize<PointToolProbeNodeOptions, SelfOptions, PathOptions>()( {
+
+      // SelfOptions
       size: new Dimension2( 18, 21 ),
+
+      // PathOptions
       fill: GLColors.POINT_TOOL_COLOR,
       stroke: Color.grayColor( 135 )
-    }, options );
+    }, providedOptions );
 
-    const shape = new Shape()
+    const triangleShape = new Shape()
       .moveTo( 0, 0 )
       .lineTo( options.size.width, 0 )
       .lineTo( options.size.width / 2, options.size.height )
       .close();
 
-    super( shape, options );
+    super( triangleShape, options );
   }
 }
 

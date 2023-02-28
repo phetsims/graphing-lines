93db3f49b3676dda0000c2d717453743f1604583
pixelzoom <cmalley@pixelzoom.com>
2023-02-28 09:25:22 -0700

    convert to TypeScript

--- js/linegame/view/PlayNode.ts
+++ js/linegame/view/PlayNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2013-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Portion of the scenegraph that corresponds to the 'play' game phase. (See GamePhase.PLAY)
  * Displays the scoreboard and current challenge.
@@ -9,13 +8,17 @@
  */
 
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import Bounds2 from '../../../../dot/js/Bounds2.js';
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import { Node, Rectangle } from '../../../../scenery/js/imports.js';
 import FiniteStatusBar from '../../../../vegas/js/FiniteStatusBar.js';
+import GameAudioPlayer from '../../../../vegas/js/GameAudioPlayer.js';
 import ScoreDisplayLabeledNumber from '../../../../vegas/js/ScoreDisplayLabeledNumber.js';
 import graphingLines from '../../graphingLines.js';
 import GamePhase from '../model/GamePhase.js';
+import LineGameModel from '../model/LineGameModel.js';
 import ChallengeNode from './ChallengeNode.js';
 
 // constants
@@ -23,13 +26,9 @@ const STATUS_BAR_FONT = new PhetFont( 20 );
 const STATUS_BAR_TEXT_FILL = 'white';
 
 export default class PlayNode extends Node {
-  /**
-   * @param {LineGameModel} model
-   * @param {Bounds2} layoutBounds
-   * @param {Property.<Bounds2>} visibleBoundsProperty
-   * @param {GameAudioPlayer} audioPlayer
-   */
-  constructor( model, layoutBounds, visibleBoundsProperty, audioPlayer ) {
+
+  public constructor( model: LineGameModel, layoutBounds: Bounds2, visibleBoundsProperty: TReadOnlyProperty<Bounds2>,
+                      audioPlayer: GameAudioPlayer ) {
 
     super();
 
@@ -71,19 +70,19 @@ export default class PlayNode extends Node {
 
     // Set up a new challenge
     // unlink unnecessary because PlayNode exists for the lifetime of the sim.
+    let previousChallengeNode: ChallengeNode;
     model.challengeProperty.link( challenge => {
 
       // dispose of view for previous challenge
-      const challengeNodes = challengeParent.getChildren();
-      for ( let i = 0; i < challengeNodes.length; i++ ) {
-        const challengeNode = challengeNodes[ i ];
-        assert && assert( challengeNode instanceof ChallengeNode );
-        challengeParent.removeChild( challengeNode );
-        challengeNode.dispose();
+      if ( previousChallengeNode ) {
+        challengeParent.removeChild( previousChallengeNode );
+        previousChallengeNode.dispose();
       }
 
       // add view for current challenge
-      challengeParent.addChild( challenge.createView( model, challengeSize, audioPlayer ) );
+      const challengeNode = challenge.createView( model, challengeSize, audioPlayer );
+      challengeParent.addChild( challengeNode );
+      previousChallengeNode = challengeNode;
     } );
   }
 }

b72ba7403ac13aaad11ddc6a7732e026a924d62f
pixelzoom <cmalley@pixelzoom.com>
2023-04-18 12:39:50 -0600

    convert to TypeScript

--- js/linegame/view/GraphPointSlopeNode.ts
+++ js/linegame/view/GraphPointSlopeNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2013-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Challenge graph with manipulators for point (x1,y1) and slope of the guess line.
  * The answer line is initially hidden.
@@ -16,13 +15,15 @@ import PointSlopeParameterRange from '../../pointslope/model/PointSlopeParameter
 import LineGameConstants from '../LineGameConstants.js';
 import ManipulationMode from '../model/ManipulationMode.js';
 import ChallengeGraphNode from './ChallengeGraphNode.js';
+import Challenge from '../model/Challenge.js';
+import NotALine from '../model/NotALine.js';
+import Line from '../../common/model/Line.js';
 
 export default class GraphPointSlopeNode extends ChallengeGraphNode {
 
-  /**
-   * @param {Challenge} challenge
-   */
-  constructor( challenge ) {
+  private readonly disposeGraphPointSlopeNode: () => void;
+
+  public constructor( challenge: Challenge ) {
 
     super( challenge );
 
@@ -32,12 +33,15 @@ export default class GraphPointSlopeNode extends ChallengeGraphNode {
     const pointSlopeParameterRange = new PointSlopeParameterRange();
     const x1RangeProperty = new Property( challenge.graph.xRange );
     const y1RangeProperty = new Property( challenge.graph.yRange );
+    // @ts-expect-error
     const riseRangeProperty = new Property( pointSlopeParameterRange.rise( challenge.guessProperty.value, challenge.graph ) );
+    // @ts-expect-error
     const runRangeProperty = new Property( pointSlopeParameterRange.run( challenge.guessProperty.value, challenge.graph ) );
 
     const manipulatorRadius = challenge.modelViewTransform.modelToViewDeltaX( LineGameConstants.MANIPULATOR_RADIUS );
 
     // point manipulator
+    // @ts-expect-error
     const pointManipulator = new X1Y1Manipulator( manipulatorRadius, challenge.guessProperty, x1RangeProperty, y1RangeProperty, challenge.modelViewTransform, true /* constantSlope */ );
     const pointIsVariable = ( challenge.manipulationMode === ManipulationMode.POINT || challenge.manipulationMode === ManipulationMode.POINT_SLOPE );
     if ( pointIsVariable ) {
@@ -45,6 +49,7 @@ export default class GraphPointSlopeNode extends ChallengeGraphNode {
     }
 
     // slope manipulator
+    // @ts-expect-error
     const slopeManipulator = new SlopeManipulator( manipulatorRadius, challenge.guessProperty, riseRangeProperty, runRangeProperty, challenge.modelViewTransform );
     const slopeIsVariable = ( challenge.manipulationMode === ManipulationMode.SLOPE || challenge.manipulationMode === ManipulationMode.POINT_SLOPE );
     if ( slopeIsVariable ) {
@@ -52,23 +57,25 @@ export default class GraphPointSlopeNode extends ChallengeGraphNode {
     }
 
     // Sync with the guess
-    const guessObserver = line => {
+    const guessObserver = ( line: Line | NotALine ) => {
+      assert && assert( line instanceof Line ); // eslint-disable-line no-simple-type-checking-assertions
+      if ( line instanceof Line ) {
 
-      // move the manipulators
-      pointManipulator.translation = challenge.modelViewTransform.modelToViewXY( line.x1, line.y1 );
-      slopeManipulator.translation = challenge.modelViewTransform.modelToViewXY( line.x2, line.y2 );
+        // move the manipulators
+        pointManipulator.translation = challenge.modelViewTransform.modelToViewXY( line.x1, line.y1 );
+        slopeManipulator.translation = challenge.modelViewTransform.modelToViewXY( line.x2, line.y2 );
 
-      // adjust ranges
-      if ( challenge.manipulationMode === ManipulationMode.POINT_SLOPE ) {
-        x1RangeProperty.value = pointSlopeParameterRange.x1( line, challenge.graph );
-        y1RangeProperty.value = pointSlopeParameterRange.y1( line, challenge.graph );
-        riseRangeProperty.value = pointSlopeParameterRange.rise( line, challenge.graph );
-        runRangeProperty.value = pointSlopeParameterRange.run( line, challenge.graph );
+        // adjust ranges
+        if ( challenge.manipulationMode === ManipulationMode.POINT_SLOPE ) {
+          x1RangeProperty.value = pointSlopeParameterRange.x1( line, challenge.graph );
+          y1RangeProperty.value = pointSlopeParameterRange.y1( line, challenge.graph );
+          riseRangeProperty.value = pointSlopeParameterRange.rise( line, challenge.graph );
+          runRangeProperty.value = pointSlopeParameterRange.run( line, challenge.graph );
+        }
       }
     };
     challenge.guessProperty.link( guessObserver ); // unlink in dispose
 
-    // @private called by dispose
     this.disposeGraphPointSlopeNode = () => {
       pointManipulator.dispose();
       slopeManipulator.dispose();
@@ -76,11 +83,7 @@ export default class GraphPointSlopeNode extends ChallengeGraphNode {
     };
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     this.disposeGraphPointSlopeNode();
     super.dispose();
   }

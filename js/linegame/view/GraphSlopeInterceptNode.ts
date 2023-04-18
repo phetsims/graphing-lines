c2dfe98048ed5d4cd3eca1b48372a6bb9ae58e4f
pixelzoom <cmalley@pixelzoom.com>
2023-04-18 12:36:58 -0600

    convert to TypeScript

--- js/linegame/view/GraphSlopeInterceptNode.ts
+++ js/linegame/view/GraphSlopeInterceptNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2013-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Challenge graph with manipulators for slope and y-intercept of the guess line.
  * The answer line is initially hidden.
@@ -16,13 +15,15 @@ import SlopeInterceptParameterRange from '../../slopeintercept/model/SlopeInterc
 import LineGameConstants from '../LineGameConstants.js';
 import ManipulationMode from '../model/ManipulationMode.js';
 import ChallengeGraphNode from './ChallengeGraphNode.js';
+import Challenge from '../model/Challenge.js';
+import NotALine from '../model/NotALine.js';
+import Line from '../../common/model/Line.js';
 
 export default class GraphSlopeInterceptNode extends ChallengeGraphNode {
 
-  /**
-   * @param {Challenge} challenge
-   */
-  constructor( challenge ) {
+  private readonly disposeGraphSlopeInterceptNode: () => void;
+
+  public constructor( challenge: Challenge ) {
 
     super( challenge );
 
@@ -30,13 +31,16 @@ export default class GraphSlopeInterceptNode extends ChallengeGraphNode {
 
     // dynamic ranges
     const parameterRange = new SlopeInterceptParameterRange();
+    // @ts-expect-error
     const riseRangeProperty = new Property( parameterRange.rise( challenge.guessProperty.value, challenge.graph ) );
+    // @ts-expect-error
     const runRangeProperty = new Property( parameterRange.run( challenge.guessProperty.value, challenge.graph ) );
     const y1RangeProperty = new Property( challenge.graph.yRange );
 
     const manipulatorRadius = challenge.modelViewTransform.modelToViewDeltaX( LineGameConstants.MANIPULATOR_RADIUS );
 
     // intercept manipulator
+    // @ts-expect-error
     const yInterceptManipulator = new YInterceptManipulator( manipulatorRadius, challenge.guessProperty, y1RangeProperty, challenge.modelViewTransform );
     const interceptIsVariable = ( challenge.manipulationMode === ManipulationMode.INTERCEPT || challenge.manipulationMode === ManipulationMode.SLOPE_INTERCEPT );
     if ( interceptIsVariable ) {
@@ -44,6 +48,7 @@ export default class GraphSlopeInterceptNode extends ChallengeGraphNode {
     }
 
     // slope manipulator
+    // @ts-expect-error
     const slopeManipulator = new SlopeManipulator( manipulatorRadius, challenge.guessProperty, riseRangeProperty, runRangeProperty, challenge.modelViewTransform );
     const slopeIsVariable = ( challenge.manipulationMode === ManipulationMode.SLOPE || challenge.manipulationMode === ManipulationMode.SLOPE_INTERCEPT );
     if ( slopeIsVariable ) {
@@ -51,21 +56,23 @@ export default class GraphSlopeInterceptNode extends ChallengeGraphNode {
     }
 
     // Sync with the guess
-    const guessObserver = line => {
+    const guessObserver = ( line: Line | NotALine ) => {
+      assert && assert( line instanceof Line ); // eslint-disable-line no-simple-type-checking-assertions
+      if ( line instanceof Line ) {
 
-      // move the manipulators
-      slopeManipulator.translation = challenge.modelViewTransform.modelToViewXY( line.x2, line.y2 );
-      yInterceptManipulator.translation = challenge.modelViewTransform.modelToViewXY( line.x1, line.y1 );
+        // move the manipulators
+        slopeManipulator.translation = challenge.modelViewTransform.modelToViewXY( line.x2, line.y2 );
+        yInterceptManipulator.translation = challenge.modelViewTransform.modelToViewXY( line.x1, line.y1 );
 
-      // adjust ranges
-      if ( challenge.manipulationMode === ManipulationMode.SLOPE_INTERCEPT ) {
-        riseRangeProperty.value = parameterRange.rise( line, challenge.graph );
-        y1RangeProperty.value = parameterRange.y1( line, challenge.graph );
+        // adjust ranges
+        if ( challenge.manipulationMode === ManipulationMode.SLOPE_INTERCEPT ) {
+          riseRangeProperty.value = parameterRange.rise( line, challenge.graph );
+          y1RangeProperty.value = parameterRange.y1( line, challenge.graph );
+        }
       }
     };
     challenge.guessProperty.link( guessObserver ); // unlink in dispose
 
-    // @private called by dispose
     this.disposeGraphSlopeInterceptNode = () => {
       yInterceptManipulator.dispose();
       slopeManipulator.dispose();
@@ -73,11 +80,7 @@ export default class GraphSlopeInterceptNode extends ChallengeGraphNode {
     };
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     this.disposeGraphSlopeInterceptNode();
     super.dispose();
   }

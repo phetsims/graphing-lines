d8b836295dcf985398ee273ec719254337ae5c03
pixelzoom <cmalley@pixelzoom.com>
2024-01-22 11:08:17 -0700

    remove static strings from GraphingLinesStrings, https://github.com/phetsims/graphing-lines/issues/140

--- js/GraphingLinesStrings.ts
+++ js/GraphingLinesStrings.ts
@@ -10,72 +10,42 @@ import graphingLines from './graphingLines.js';
 
 type StringsType = {
   'graphing-lines': {
-    'title': string;
     'titleStringProperty': LocalizedStringProperty;
   };
-  'eraseLines': string;
   'eraseLinesStringProperty': LocalizedStringProperty;
-  'saveLine': string;
   'saveLineStringProperty': LocalizedStringProperty;
-  'slope': string;
   'slopeStringProperty': LocalizedStringProperty;
-  'slopeUndefined': string;
   'slopeUndefinedStringProperty': LocalizedStringProperty;
-  'graphTheLine': string;
   'graphTheLineStringProperty': LocalizedStringProperty;
-  'makeTheEquation': string;
   'makeTheEquationStringProperty': LocalizedStringProperty;
-  'setTheSlope': string;
   'setTheSlopeStringProperty': LocalizedStringProperty;
-  'setTheYIntercept': string;
   'setTheYInterceptStringProperty': LocalizedStringProperty;
-  'setThePoint': string;
   'setThePointStringProperty': LocalizedStringProperty;
-  'putPointsOnLine': string;
   'putPointsOnLineStringProperty': LocalizedStringProperty;
-  'lineToGraph': string;
   'lineToGraphStringProperty': LocalizedStringProperty;
-  'yourLine': string;
   'yourLineStringProperty': LocalizedStringProperty;
-  'yourEquation': string;
   'yourEquationStringProperty': LocalizedStringProperty;
-  'aCorrectEquation': string;
   'aCorrectEquationStringProperty': LocalizedStringProperty;
-  'undefined': string;
   'undefinedStringProperty': LocalizedStringProperty;
-  'notALine': string;
   'notALineStringProperty': LocalizedStringProperty;
   'symbol': {
-    'x': string;
     'xStringProperty': LocalizedStringProperty;
-    'y': string;
     'yStringProperty': LocalizedStringProperty;
-    'slope': string;
     'slopeStringProperty': LocalizedStringProperty;
-    'intercept': string;
     'interceptStringProperty': LocalizedStringProperty;
   };
   'screen': {
-    'slope': string;
     'slopeStringProperty': LocalizedStringProperty;
-    'slopeIntercept': string;
     'slopeInterceptStringProperty': LocalizedStringProperty;
-    'pointSlope': string;
     'pointSlopeStringProperty': LocalizedStringProperty;
-    'lineGame': string;
     'lineGameStringProperty': LocalizedStringProperty;
   };
   'point': {
-    'XY': string;
     'XYStringProperty': LocalizedStringProperty;
-    'unknown': string;
     'unknownStringProperty': LocalizedStringProperty;
   };
-  'slopeIs': string;
   'slopeIsStringProperty': LocalizedStringProperty;
-  'chooseYourLevel': string;
   'chooseYourLevelStringProperty': LocalizedStringProperty;
-  'pattern_Level_0': string;
   'pattern_Level_0StringProperty': LocalizedStringProperty;
 };
 

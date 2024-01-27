a58fd8d52c8a19d0cb921de38ae04bf58a06e410
phet-dev <phetadmin@gmail.com>
2024-01-27 03:14:08 -0700

    update copyright dates from daily grunt work

--- js/linegame/view/ResultsNode.ts
+++ js/linegame/view/ResultsNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2023, University of Colorado Boulder
+// Copyright 2013-2024, University of Colorado Boulder
 
 /**
  * Portion of the scene graph that corresponds to the 'results' game phase. (See GamePhase.RESULTS)

83e3c271c4726691cb284dad348e45b53d32534c
pixelzoom <cmalley@pixelzoom.com>
2024-02-03 13:26:37 -0700

    grunt modulify, replace png files with usa/*.svg files, https://github.com/phetsims/graphing-lines/issues/143

--- js/linegame/view/LineGameScreenView.ts
+++ js/linegame/view/LineGameScreenView.ts
@@ -7,12 +7,12 @@
  */
 
 import Tandem from '../../../../tandem/js/Tandem.js';
-import level1_png from '../../../images/level1_png.js';
-import level2_png from '../../../images/level2_png.js';
-import level3_png from '../../../images/level3_png.js';
-import level4_png from '../../../images/level4_png.js';
-import level5_png from '../../../images/level5_png.js';
-import level6_png from '../../../images/level6_png.js';
+import level1_svg from '../../../images/usa/level1_svg.js';
+import level2_svg from '../../../images/usa/level2_svg.js';
+import level3_svg from '../../../images/usa/level3_svg.js';
+import level4_svg from '../../../images/usa/level4_svg.js';
+import level5_svg from '../../../images/usa/level5_svg.js';
+import level6_svg from '../../../images/usa/level6_svg.js';
 import graphingLines from '../../graphingLines.js';
 import LineGameModel from '../model/LineGameModel.js';
 import BaseGameScreenView from './BaseGameScreenView.js';
@@ -23,7 +23,7 @@ export default class LineGameScreenView extends BaseGameScreenView {
   public constructor( model: LineGameModel, tandem: Tandem ) {
 
     // Images for the level-selection buttons, ordered by level
-    const levelImages = [ level1_png, level2_png, level3_png, level4_png, level5_png, level6_png ];
+    const levelImages = [ level1_svg, level2_svg, level3_svg, level4_svg, level5_svg, level6_svg ];
 
     // functions that create nodes for the game reward, ordered by level
     const rewardNodeFunctions = [

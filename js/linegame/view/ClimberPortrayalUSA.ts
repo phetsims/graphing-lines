6f62af338919c18056d49abcf1727e2dd6a7565f
pixelzoom <cmalley@pixelzoom.com>
2024-02-08 00:59:34 -0700

    Revert "move all ClimberPortrayal instances to view/portrayals/, https://github.com/phetsims/graphing-lines/issues/143"
    This reverts commit 96a817b25fb348717391efdeff6bdc1bda4a0bf3.

---
+++ js/linegame/view/ClimberPortrayalUSA.ts
@@ -0,0 +1,25 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * This file instantiates the USA region and culture portrayals.
+ *
+ * @author Luisa Vargas
+ */
+
+import ClimberPortrayal from './portrayals/ClimberPortrayal.js';
+import JoistStrings from '../../../../joist/js/JoistStrings.js';
+import { USA_REGION_AND_CULTURE_ID } from '../../../../joist/js/preferences/RegionAndCulturePortrayal.js';
+import level1_svg from '../../../images/usa/level1_svg.js';
+import level2_svg from '../../../images/usa/level2_svg.js';
+import level3_svg from '../../../images/usa/level3_svg.js';
+import level4_svg from '../../../images/usa/level4_svg.js';
+import level5_svg from '../../../images/usa/level5_svg.js';
+import level6_svg from '../../../images/usa/level6_svg.js';
+
+const ClimberPortrayalUSA = new ClimberPortrayal(
+  JoistStrings.preferences.tabs.localization.regionAndCulture.portrayalSets.unitedStatesOfAmericaStringProperty,
+  USA_REGION_AND_CULTURE_ID,
+  [ level1_svg, level2_svg, level3_svg, level4_svg, level5_svg, level6_svg ]
+);
+
+export default ClimberPortrayalUSA;

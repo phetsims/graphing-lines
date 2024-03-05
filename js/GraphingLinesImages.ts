ab0e4f5bb9d254b63337ae47069b11d3d25fffc6
pixelzoom <cmalley@pixelzoom.com>
2024-03-04 17:55:08 -0700

    regenerate images file with correct case, https://github.com/phetsims/graphing-lines/issues/143

---
+++ js/GraphingLinesImages.ts
@@ -0,0 +1,91 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import LocalizedImageProperty from '../../joist/js/i18n/LocalizedImageProperty.js';
+import graphingLines from './graphingLines.js';
+import africaLevel1_svg from '../images/africaLevel1_svg.js';
+import africaLevel2_svg from '../images/africaLevel2_svg.js';
+import africaLevel3_svg from '../images/africaLevel3_svg.js';
+import africaLevel4_svg from '../images/africaLevel4_svg.js';
+import africaLevel5_svg from '../images/africaLevel5_svg.js';
+import africaLevel6_svg from '../images/africaLevel6_svg.js';
+import africaModestLevel4_svg from '../images/africaModestLevel4_svg.js';
+import africaModestLevel6_svg from '../images/africaModestLevel6_svg.js';
+import asiaLevel1_svg from '../images/asiaLevel1_svg.js';
+import asiaLevel2_svg from '../images/asiaLevel2_svg.js';
+import asiaLevel3_svg from '../images/asiaLevel3_svg.js';
+import asiaLevel4_svg from '../images/asiaLevel4_svg.js';
+import asiaLevel5_svg from '../images/asiaLevel5_svg.js';
+import asiaLevel6_svg from '../images/asiaLevel6_svg.js';
+import latinAmericaLevel1_svg from '../images/latinAmericaLevel1_svg.js';
+import latinAmericaLevel2_svg from '../images/latinAmericaLevel2_svg.js';
+import latinAmericaLevel3_svg from '../images/latinAmericaLevel3_svg.js';
+import latinAmericaLevel4_svg from '../images/latinAmericaLevel4_svg.js';
+import latinAmericaLevel5_svg from '../images/latinAmericaLevel5_svg.js';
+import latinAmericaLevel6_svg from '../images/latinAmericaLevel6_svg.js';
+import oceaniaLevel3_svg from '../images/oceaniaLevel3_svg.js';
+import oceaniaLevel5_svg from '../images/oceaniaLevel5_svg.js';
+import usaLevel1_svg from '../images/usaLevel1_svg.js';
+import usaLevel2_svg from '../images/usaLevel2_svg.js';
+import usaLevel3_svg from '../images/usaLevel3_svg.js';
+import usaLevel4_svg from '../images/usaLevel4_svg.js';
+import usaLevel5_svg from '../images/usaLevel5_svg.js';
+import usaLevel6_svg from '../images/usaLevel6_svg.js';
+
+const GraphingLinesImages = {
+  level1ImageProperty: new LocalizedImageProperty( 'level1', {
+    usa: usaLevel1_svg,
+    africa: africaLevel1_svg,
+    africaModest: africaLevel1_svg,
+    asia: asiaLevel1_svg,
+    latinAmerica: latinAmericaLevel1_svg,
+    oceania: asiaLevel1_svg
+  } ),
+  level2ImageProperty: new LocalizedImageProperty( 'level2', {
+    usa: usaLevel2_svg,
+    africa: africaLevel2_svg,
+    africaModest: africaLevel2_svg,
+    asia: asiaLevel2_svg,
+    latinAmerica: latinAmericaLevel2_svg,
+    oceania: asiaLevel2_svg
+  } ),
+  level3ImageProperty: new LocalizedImageProperty( 'level3', {
+    usa: usaLevel3_svg,
+    africa: africaLevel3_svg,
+    africaModest: africaLevel3_svg,
+    asia: asiaLevel3_svg,
+    latinAmerica: latinAmericaLevel3_svg,
+    oceania: oceaniaLevel3_svg
+  } ),
+  level4ImageProperty: new LocalizedImageProperty( 'level4', {
+    usa: usaLevel4_svg,
+    africa: africaLevel4_svg,
+    africaModest: africaModestLevel4_svg,
+    asia: asiaLevel4_svg,
+    latinAmerica: latinAmericaLevel4_svg,
+    oceania: asiaLevel4_svg
+  } ),
+  level5ImageProperty: new LocalizedImageProperty( 'level5', {
+    usa: usaLevel5_svg,
+    africa: africaLevel5_svg,
+    africaModest: africaLevel5_svg,
+    asia: asiaLevel5_svg,
+    latinAmerica: latinAmericaLevel5_svg,
+    oceania: oceaniaLevel5_svg
+  } ),
+  level6ImageProperty: new LocalizedImageProperty( 'level6', {
+    usa: usaLevel6_svg,
+    africa: africaLevel6_svg,
+    africaModest: africaModestLevel6_svg,
+    asia: asiaLevel6_svg,
+    latinAmerica: latinAmericaLevel6_svg,
+    oceania: asiaLevel6_svg
+  } )
+};
+
+graphingLines.register( 'GraphingLinesImages', GraphingLinesImages );
+
+export default GraphingLinesImages;

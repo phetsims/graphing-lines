a6a47a8c5fb247e3b2a59e4e32ff823f63ff834b
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:05 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for graphing-lines.
+ * ESLint configuration for graphing-lines.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
